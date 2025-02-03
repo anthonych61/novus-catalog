c995a9d60624fa8bef813e7824a4a6f246e25ff7
Steven Cherry <anthonych61@gmail.com>
2025-02-03 11:55:37 -0500

    Leetcode solutions submitted: 2/3/2025

---
+++ Leetcode Solutions/Greedy/Lemonade Change/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+// At a lemonade stand, each lemonade costs $5. Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills). 
+// Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill. You must provide the correct change to each customer so that the net transaction is that the customer pays $5.
+// Note that you don't have any change in hand at first.
+// Given an integer array bills where bills[i] is the bill the ith customer pays, return true if you can provide every customer with correct change, or false otherwise.
+// Complexity Analysis:
+// Time Complexity: O(N), where N is the length of bills.
+// Space Complexity: O(1).
+namespace Lemonade_Change
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Boolean flag = true;
+            int[] bills = new int[] {5,5,5,10,20}; // {5,5,5,10,5,5,10,20,20,20}; 
+            int fiveDollars = 0, tenDollars = 0;      
+           
+            for(int i = 0; i <= bills.Length-1; i++)
+            {                   
+                if(bills[i] == 5)   // keep track of $5 bills received
+                {
+                    fiveDollars++;
+                }
+                else if(bills[i] == 10)     // keep track of $10 bills received
+                {
+                    tenDollars++;
+                    
+                    if(fiveDollars >= 1)
+                    {
+                        fiveDollars--; // give back $5 change
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                    
+                }
+                else // otherwise
+                {                    
+                    if(fiveDollars >= 1 && tenDollars >= 1)
+                    {
+                        fiveDollars--; // give back $5 | $10 change
+                        tenDollars--;
+                    }
+                    else if(fiveDollars >= 3)
+                    {
+                        fiveDollars-=3; // give back $5 change
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+                    
+                }                     
+                               
+            }           
+            Console.WriteLine(flag);           
+          
+        }
+    }
+}
---
+++ Leetcode Solutions/Sliding Window/Repeated Substring Pattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+// Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+namespace Repeated_Substring_Pattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s =  "abaababaab"; //"abcabc"; //"aaa"; //"babbabbabbabbab";  //"aba";  // "abcabcabcabc";         
+        int half = s.Length/2;
+        StringBuilder sb = new StringBuilder();
+        string substr = s.Substring(0, half);
+        
+        for(int i = 1; i <= substr.Length; i++) 
+        {
+            if (s.Length % i == 0) 
+            {
+                sb = new StringBuilder();
+                for(int j = 0; j < s.Length  / i; j++) 
+                    sb.Append(substr.Substring(0, i));   
+             }                                       
+        }  
+
+        if(sb.ToString() == s) 
+        {
+            Console.WriteLine(true);
+        }
+        else
+        {
+            Console.WriteLine(false);
+        }          
+
+    }
+}
