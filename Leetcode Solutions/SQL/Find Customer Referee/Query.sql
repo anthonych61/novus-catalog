a6e9bb4925107367d5b9c76be8ad942d44fb5db0
Steven Cherry <anthonych61@gmail.com>
2025-04-09 15:03:30 -0400

    Completed Find Customer Referee

--- Leetcode Solutions/SQL/Find Customer Referee/Query.sql
+++ Leetcode Solutions/SQL/Find Customer Referee/Query.sql
@@ -1,2 +1,2 @@
 /* Write your T-SQL query statement below */
-SELECT name FROM Customer WHERE (referee_id <> 2) OR (referee_id IS NULL);
+SELECT name FROM Customer WHERE (referee_id <> 2 OR referee_id IS NULL)
